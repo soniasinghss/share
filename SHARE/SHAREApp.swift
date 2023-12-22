4b84609c575c0dcb384ad4633bcf28cd35e4d67b
Sonia Singh <sonia_singh20@hotmail.com>
2023-12-22 16:43:36 -0500

    Initial Commit

---
+++ SHARE/SHAREApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHAREApp.swift
+//  SHARE
+//
+//  Created by Sonia Singh on 2023-12-22.
+//
+
+import SwiftUI
+
+@main
+struct SHAREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
